76b2c504f25565e3f82171080e9aa0237a3df1a6
GabriellMatias <gabriel.goiasat@gmail.com>
2023-05-25 20:34:15 -0300

    :rocket: Finished challenger

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,36 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(
+      user_id, 
+      { relations: ["games"] }
+    );
+
+    if (!user) {
+      throw new Error("User does not exist!");
+    }
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return await this.repository.query(`
+      SELECT * 
+      FROM users
+      ORDER BY first_name ASC
+    `); 
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return await this.repository.query(`
+      SELECT * 
+      FROM users
+      WHERE LOWER(first_name) = LOWER('${first_name}') AND LOWER(last_name) = LOWER('${last_name}')
+    `); 
   }
 }
